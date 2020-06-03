ed7e8d26ceebb13f3259be8b9f531bec8d67ce06
Eliza Regas <elizaregas@gmail.com>
2020-06-03 18:00:36 -0400

    input info being pushed into arrays

--- develop/script.js
+++ develop/script.js
@@ -1,15 +1,10 @@
 // variables
 var generateBtn = document.querySelector("#generate");
 var acceptedPasswordLength = [];
-
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
+var selectedCapitalLetters = [];
+var selectedLowerCaseLetters = [];
+var selectedNumbers = [];
+var selectedSpecialCharacters = [];
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
@@ -24,22 +19,19 @@ function generatePassword() {
   var numberCheck = isNaN(requestedPasswordLength);
   if (numberCheck === false) {
     acceptedPasswordLength.push(requestedPasswordLength);
-    console.log(acceptedPasswordLength);
   } else {
     alert("Not a valid input. Please try again.");
-    console.log("nope");
   }
   // WHEN prompted for the length of the password
   // THEN I choose a length of at least 8 characters and no more than 128 characters
   if (requestedPasswordLength > 129 || requestedPasswordLength < 8) {
     alert("Passwords must be between 8 and 128 characters. Please try again");
-    console.log("try again");
   }
 
   var finalPasswordLength = acceptedPasswordLength.pop();
 
+  // condition that checks the password length before moving forward
   if (finalPasswordLength < 129 && finalPasswordLength > 7) {
-    console.log("running gatherInfo");
     gatherInfo();
   }
   function gatherInfo() {
@@ -50,28 +42,41 @@ function generatePassword() {
 
     // prompt information
     var capitalLetters = confirm("Do you want to include capital letters?");
-
     if (capitalLetters) {
       alert("Capital letters will be included!");
+      var capitalLetterArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+      selectedCapitalLetters.push(capitalLetterArray)
+      console.log(selectedCapitalLetters);
     } else {
       alert("No capital letters for you!");
     }
 
     var lowerCaseLetters = confirm("Do you want to include lower case letters?");
-
     if (lowerCaseLetters) {
+      alert("Lower case letters coming right up!");
+      var lowerCaseLetterArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+      selectedLowerCaseLetters.push(lowerCaseLetterArray)
+      console.log(selectedLowerCaseLetters);
+    } else {
+      alert("Lower case letters just aren't your thing, huh?!");
     }
 
     var numbers = confirm("Do you want to include numbers?");
-
     if (numbers) {
+      alert("You can count on it!");
+      var numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+      selectedNumbers.push(numberArray)
+      console.log(selectedNumbers);
+    } else {
+      alert("All of the cool kids are doing it, but whatever...");
     }
 
-    var specialCharacters = confirm(
-      "Do you want to include special characters?"
-    );
-
+    var specialCharacters = confirm("Do you want to include special characters?");
     if (specialCharacters) {
+      alert("You're special, just like your characters!");
+      var specialCharacterArray = []
+    } else {
+      alert("Boooooorrrriiiiinnnnnggg!");
     }
   }
 }
@@ -84,3 +89,12 @@ function generatePassword() {
 
 // WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page
+
+// Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
